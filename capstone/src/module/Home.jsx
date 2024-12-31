3e7d74104774b376a9f9b33cf146ff7fd2a33ca5
Sidiq Daniel <sidinfo21@gmail.com>
2024-12-31 18:06:43 -0500

    Add files via upload

---
+++ capstone/src/module/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Specials from '../components/Specials';
+import Testimonials from '../components/Testimonials';
+import Story from '../components/Story';
+
+const Home = () => (
+  <main>
+    <Hero />
+    <Specials />
+    <Testimonials />
+    <Story />
+  </main>
+);
+
+export default Home;
